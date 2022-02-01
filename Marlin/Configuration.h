dfa97b3744c256f9a8a2e8ed35095ba86d64fbe9
Andrea Pirazzini <info@help3d.it>
2022-02-01 16:01:43 +0100

    Disable Preheat

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1563,7 +1563,7 @@
  */
 #define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
-  #define LEVELING_NOZZLE_TEMP 180   // (°C) Only applies to E0 at this time
+  //#define LEVELING_NOZZLE_TEMP 180   // (°C) Only applies to E0 at this time
   #define LEVELING_BED_TEMP     60
 #endif
 
