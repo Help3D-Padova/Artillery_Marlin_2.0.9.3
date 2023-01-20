611cf150f0622b163aab870f45f7160aed2de492
Andrea Pirazzini <info@help3d.it>
2023-01-20 17:46:14 +0100

    Support GH drivers

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(H3D_X2_2.0.9.3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(H3D_X2_GH_2.0.9.3)" // Who made the changes.
 #define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
